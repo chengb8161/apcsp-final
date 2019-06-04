6990ff6fa960bcae38d79ba225cbe2c599ac0df6
Bryan Cheng <bchengsd@gmail.com>
2019-06-04 15:18:36 +0000

    Decimals

--- finaltest.c
+++ finaltest.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-int add(int smaller, int larger) 
+float add(float smaller, float larger) 
 {
-  int solution = (smaller + larger);
+  float solution = (smaller + larger);
   return solution;
 }
 
-int sub(int smaller, int larger) 
+float sub(float smaller, float larger) 
 {
-  int solution = (smaller - larger);
+  float solution = (smaller - larger);
   return solution;
 }
 
-int mult(int smaller, int larger) 
+float mult(float smaller, float larger) 
 {
-  int solution = (smaller * larger);
+  float solution = (smaller * larger);
   return solution;
 }
 
-float div(int smaller, float larger)     //we need the second to be float for it to work
+float div(float smaller, float larger)     //we need the second to be float for it to work
 {
   float solution = (smaller / larger);
   return solution;
@@ -41,36 +41,36 @@ int main(int argc, char* argv[])
   }
 
   // at this point we know we have two args, let's check that they are ints
- int smaller;
+ float smaller;
   // ssscanf scans a string for a format - in this case an integer (%f) and returns
   // the number of items found
-  int found = sscanf(argv[1], "%d", &smaller);
+  int found = sscanf(argv[1], "%f", &smaller);
   if (found != 1)
   {
-    printf("first arg is not an integer, enter two ints\n");
+    printf("first arg is not an integer, enter two floats\n");
     return 1;
   }
 
-  int larger;
-  found = sscanf(argv[2], "%d", &larger);
+  float larger;
+  found = sscanf(argv[2], "%f", &larger);
   if (found != 1)
   {
-    printf("second arg is not an integer, enter two ints\n");
+    printf("second arg is not an integer, enter two floats\n");
     return 1;
   }
 
 
-int addition = add(smaller, larger);
-printf("%d + %d is equal to %d\n", smaller, larger, addition);
+float addition = add(smaller, larger);
+printf("%f + %f is equal to %f\n", smaller, larger, addition);
 
-int subtraction = sub(smaller, larger);
-printf("%d - %d is equal to %d\n", smaller, larger, subtraction);
+float subtraction = sub(smaller, larger);
+printf("%f - %f is equal to %f\n", smaller, larger, subtraction);
 
-int multiplication = mult(smaller, larger);
-printf("%d x %d is equal to %d\n", smaller, larger, multiplication);
+float multiplication = mult(smaller, larger);
+printf("%f x %f is equal to %f\n", smaller, larger, multiplication);
 
 float division = div(smaller, larger);
-printf("%d / %d is equal to %f\n", smaller, larger, division);
+printf("%f / %f is equal to %f\n", smaller, larger, division);
 
 }
   
