ec4f56779089b634524e557e291ba2c6bce49968
matteogristina <33038970+matteogristina@users.noreply.github.com>
2019-06-04 08:22:05 -0700

    Update finaltest.php

--- finaltest.php
+++ finaltest.php
@@ -12,11 +12,12 @@
 
     <?php
        // define variables and set to empty values
-       $arg1 = $arg2 = $output = $retc = "";
+       $arg1 = $arg2 = $arg3 = $output = $retc = "";
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $arg1 = test_input($_POST["arg1"]);
          $arg2 = test_input($_POST["arg2"]);
-         exec("/usr/lib/cgi-bin/sp1b/finaltest " . $arg1 . " " . $arg2, $output, $retc); 
+         $arg3 = test_input($_POST["arg3"]);
+         exec("/usr/lib/cgi-bin/sp1b/finaltest " . $arg1 . " " . $arg2, $arg3, $output, $retc); 
        }
        function test_input($data) {
          $data = trim($data);
@@ -29,6 +30,7 @@
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       Input 1: <input type="text" name="arg1"><br>
       Input 2: <input type="text" name="arg2"><br>
+      Input 3: <input type="text" name="arg3"><br>
       <br>
       <input type="submit" value="Go!">
     </form>
@@ -40,6 +42,8 @@
          echo $arg1;
          echo "<br>";
          echo $arg2;
+         echo "<br>";
+         echo $arg3;
          echo "<br>";
        
          echo "<h2>Program Output (an array):</h2>";
