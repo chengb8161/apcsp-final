059d48c69263f9aa4f2770d261f49f0b5d398137
matteogristina <33038970+matteogristina@users.noreply.github.com>
2019-06-06 08:55:56 -0700

    Update finaltest.php

--- finaltest.php
+++ finaltest.php
@@ -12,13 +12,12 @@
 
     <?php
        // define variables and set to empty values
-       $arg1 = $arg2 = $arg3 = $arg4 = $output = $retc = "";
+       $arg1 = $arg2 = $arg3 = $output = $retc = "";
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $arg1 = test_input($_POST["arg1"]);
          $arg2 = test_input($_POST["arg2"]);
          $arg3 = test_input($_POST["arg3"]);
-         $arg4 = test_input($_POST["arg4"]);
-         exec("/usr/lib/cgi-bin/sp1a/finaltest " . $arg1 . " " . $arg2 . " " . $arg3 . " " . $arg4, $output, $retc); 
+         exec("/usr/lib/cgi-bin/sp1a/finaltest " . $arg1 . " " . $arg2 . " " . $arg3, $output, $retc); 
        }
        function test_input($data) {
          $data = trim($data);
@@ -32,7 +31,6 @@
       Number: <input type="text" name="arg1"><br>
       Operation: <input type="text" name="arg2"><br>
       Number: <input type="text" name="arg3"><br>
-      Iterate? (Y/N): <input type="text" name="arg4"><br>
       <br>
       <input type="submit" value="Calculate!">
     </form>
@@ -46,8 +44,6 @@
          echo $arg2;
          echo "<br>";
          echo $arg3;
-         echo "<br>";
-         echo $arg4;
          echo "<br>";
        
          echo "<h2>Program Output (an array):</h2>";
